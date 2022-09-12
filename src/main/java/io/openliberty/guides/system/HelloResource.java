e96c4a8a9805af14da5fdc728ee67a3d0aa4e85c
Anthony G <ozyohthree@gmail.com>
2022-09-12 15:47:58 -0400

    updates

---
+++ src/main/java/io/openliberty/guides/system/HelloResource.java
@@ -0,0 +1,22 @@
+package io.openliberty.guides.system;
+
+// CDI
+import jakarta.enterprise.context.RequestScoped;
+import jakarta.ws.rs.GET;
+// JAX-RS
+import jakarta.ws.rs.Path;
+import jakarta.ws.rs.Produces;
+import jakarta.ws.rs.core.MediaType;
+
+@RequestScoped
+@Path("hello")
+public class HelloResource {
+
+    @GET
+    @Produces(MediaType.APPLICATION_JSON)
+    public String getGreeting() {
+        String greeting = "Hello_World";
+        return greeting;
+    }
+
+}
